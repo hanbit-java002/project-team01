137f29304c2b6f941c686fc30e2ad2bc2fc585cc
Baek-soo-hyun <100suhyun@naver.com>
2017-04-10 17:04:27 +0900

    header, footer update

--- src/js/common.js
+++ src/js/common.js
@@ -1,4 +1,25 @@
 define([
 	"bootstrap",
 ], function() {
+	function scrollUp() {
+		$(document).ready(function() {
+			var prePosition = 0;
+			$(window).on("scroll", function(event) {
+				var initPosition = $(this).scrollTop();
+				if(initPosition > prePosition) {
+					$("#scroll-up-btn").show();
+				}
+				else {
+					$("#scroll-up-btn").hide();
+				}
+			});
+		});
+		$("#scroll-up-btn").click(function() {
+			$("html, body").animate({
+				scrollTop: 0,
+			}, 400);
+		});
+	}
+
+	scrollUp();
 });
