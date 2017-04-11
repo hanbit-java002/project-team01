7436fd292f4daf937a70a1c6e246ae31adb66bc3
impact0823@naver.com <young1596>
2017-04-11 17:19:49 +0900

    market UI

--- src/js/market.js
+++ src/js/market.js
@@ -1,4 +1,11 @@
 require([
 	"common",
 ], function() {
+	$(".brand>ul>li").on("click", function () {
+		$(".brand>ul>li").removeClass("active");
+		$(this).addClass("active");
+
+		/* 버튼을 눌렀을때 실행될 상황*/
+		console.log($(this).attr("brand-name"));
+	});
 });
