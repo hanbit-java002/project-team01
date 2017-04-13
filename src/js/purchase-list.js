d66ba954bce0397f511395255de10c28de96c4e1
impact0823@naver.com <young1596>
2017-04-13 14:51:44 +0900

    product-list.less

--- src/js/purchase-list.js
+++ src/js/purchase-list.js
@@ -1,11 +1,25 @@
 require([
 	"common",
 ], function() {
-	$(".brand>ul>li").on("click", function () {
-		$(".brand>ul>li").removeClass("active");
-		$(this).addClass("active");
 
-		/* 버튼을 눌렀을때 실행될 상황*/
-		console.log($(this).attr("brand-name"));
-	});
+	var naviHandler=function () {
+
+
+	};
+
+	function naviGate(className, naviHandler) {
+		className= "."+className;
+		$(className+">ul>li").on("click", function () {
+			$(className+">ul>li").removeClass("active");
+			$(this).addClass("active");
+
+			/* 버튼을 눌렀을때 실행될 상황*/
+			console.log($(this).attr(className));
+		});
+	}
+
+
+
+	naviGate("purchase-progress", naviHandler);
+
 });
