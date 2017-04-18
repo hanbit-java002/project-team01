a022d5a320674c27230f315c5ffd39a14ee4ad85
impact0823@naver.com <young1596>
2017-04-18 10:52:30 +0900

    lists commit

--- src/js/selling-list.js
+++ src/js/selling-list.js
@@ -3,6 +3,25 @@ require([
 ], function() {
 	var common = require("common");
 
+	$("[check-box]").on("click", function () {
+		if ($(this).attr("get-checked") == "true") {
+			$(this).attr("get-checked", "false");
+		}
+		else if ($(this).attr("get-checked") == "false") {
+			$(this).attr("get-checked", "true");
+		}
+	});
+
+	$("#all-selected").on("click", function () {
+		if ($(this).attr("get-checked") == "true") {
+			$("[name='chk']").attr("get-checked", "true");
+		}
+		else if ($(this).attr("get-checked") == "false") {
+			$("[name='chk']").attr("get-checked", "false");
+		}
+
+	});
+
 	var naviHandler = function (jqElement) {
 		if ($(jqElement).attr("menu-category-detail") === "purchase-process") {
 
@@ -14,5 +33,9 @@ require([
 
 	$(".menu-category>ul>li").on("click", function () {
 		common.navigate(this, naviHandler);
+	});
+
+	$("#all-selected").on("click", function () {
+		console.log($("#all-selected").prop("checked"));
 	});
 });
