2d4b5815838ec795eaa95583c60fcc275874818f
Baek-soo-hyun <100suhyun@naver.com>
2017-04-22 19:03:48 +0900

    2017.04.22 update

--- src/js/admin-update-member.js
+++ src/js/admin-update-member.js
@@ -6,7 +6,6 @@ require([
 	//수정 버튼 클릭 시 확인 팝업 창
 	$(".btn-update").on("click", function() {
 		common.popUp("pop-up-series", "left");
-
 		$(".pop-up-series").css("position", "fixed");
 		$(".pop-up-series>.text1").text("회원 정보를 수정하시겠습니까?");
 	});
