9cc838b3d69851ce0dcf6e1c7ed57a6a1ba13dda
Baek-soo-hyun <100suhyun@naver.com>
2017-04-24 18:44:03 +0900

    market-detail update

--- src/js/market-detail.js
+++ src/js/market-detail.js
@@ -1,4 +1,18 @@
 require([
 	"common",
 ], function() {
+
+	$(".product-img-layer .carousel").carousel("pause");
+
+	/*---show product-img-layer---*/
+	$("#market-detail-img-carousel .product-img").on("click", function() {
+		$(".product-img-layer").css("display", "block");
+		$("body").css("overflow", "hidden");
+	});
+
+	/*---hide product-img-layer---*/
+	$(".product-img-layer .close-icon").on("click", function() {
+		$(".product-img-layer").css("display", "none");
+		$("body").css("overflow", "");
+	});
 });
