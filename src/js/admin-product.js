d8f26069ec6074edd5bcaa1310c4ab273f4d5545
YoungWookCho <impact0823@naver.com>
2017-05-15 18:46:15 +0900

    판매관리

--- src/js/admin-product.js
+++ src/js/admin-product.js
@@ -2,6 +2,209 @@ require([
 	"common",
 ], function() {
 	var common= require("common");
+	var rowsPerPage = 5;
+	var page =0;
+
+
+	function initCountLike(productId) {
+		$.ajax({
+			url: window._ctx.root + "/api/like/count/" + productId,
+			success: function (result) {
+				$(".product-item-list").find(".like-count").html("<span class='fa fa-heart-o'></span> " + result);
+			},
+		});
+	}
+
+	function initHits(productId) {
+		$.ajax({
+			url: window._ctx.root + "/api/hits/count/" + productId,
+			success: function (data) {
+				var hitsHTML = "<div class=\"hits\"><span class=\"fa fa-eye\"></span>"+data+"</div>";
+
+				$(".product-item-list[product-id='" + productId + "'] .hits").replaceWith(hitsHTML);
+			},
+		});
+	}
+
+	function goMarketDetail() {
+		$(".product-list>li").off();
+		/* 마켓디테일 페이지 이동*/
+		$(".product-list>li").on("click", function () {
+			var productId= $(this).attr("product-id");
+			console.log("제품아이디"+productId);
+			console.log("제품아이디"+productId);
+			console.log("제품아이디"+productId);
+			console.log("제품아이디"+productId);
+			console.log("제품아이디"+productId);
+			$.ajax({
+				url: window._ctx.root + "/api/hits/plus/" + productId,
+				success: function () {
+				},
+			});
+			var url = window._ctx.root+"/market/market-detail.html";
+			url += "?product="+productId;
+			location.href =url;
+		});
+	}
+
+	function showList(menuCategory) {
+
+		$.ajax({
+			url: window._ctx.root + "/api/admin/product/list",
+			data: {
+				menuCategory: menuCategory,
+				rowsPerPage: rowsPerPage,
+				page: page,
+			},
+			method: "POST",
+			success: function (result) {
+				console.log("메뉴카테고리 "+menuCategory);
+
+
+				var count = result.count;
+				var lastPage = parseInt(count / rowsPerPage)
+					+ (count % rowsPerPage === 0 ? 0 : 1)-1;
+				console.log("마지막페이지"+lastPage);
+				var productList ="";
+				for (var i=0; i<result.list.length; i++) {
+					$(".product-count>span").text(result.list.length);
+					var item = result.list[i];
+
+					productList += "<li class=\"product-item-list\" product-id=\""+item.product_id+"" +
+						"\" process=\""+item.selling_status+"\">";
+					productList += "    <div class=\"product-info\">";
+					productList += "        <img class=\"product-img\" src=\""+
+						/*window._ctx.root+"/img/jordan1.png"*/item.img_url+"\">";
+					productList += "        <div class=\"detail-info\">";
+					productList += "            <div class=\"product name\">";
+					productList += 						item.product_name;
+					productList += "            </div>";
+					productList += "            <div class=\"product price\">";
+					productList += "                <i class=\"fa fa-won\"></i>";
+					productList += 				common.numberWithCommas(item.price);
+					productList += "            </div>";
+					productList += "            <div class=\"product size\">";
+					productList += 					item.size;
+					if (item.category_name === "신발") {
+						productList += 	"mm";
+					}
+					else if (item.category_name === "허리") {
+						productList += 	"inch";
+					}
+					productList += "            </div>";
+					productList += "            <div class=\"product quality\">";
+					if (item.quality === "new") {
+						productList += 	"새상품";
+					}
+					else if (item.quality === "used") {
+						productList += 	"중고";
+					}
+					productList += "            </div>";
+					productList += "            <ul class=\"product dealing-mode\">";
+
+					var dealMeans = item.deal_means.split("|");
+					for (var j=0; j<dealMeans.length; j++) {
+						var dealType = dealMeans[j];
+
+						if (dealType === "direct") {
+							productList += "<li class=\"dealing-mode directly\">";
+							productList += "직접거래";
+							productList += "</li>";
+						}
+						if (dealType === "delivery") {
+							productList += "<li class=\"dealing-mode delivery\">";
+							productList += "택배거래";
+							productList += "</li>";
+						}
+					}
+
+					if (item.safe_deal === 1) {
+						productList += "<li class=\"dealing-mode safety\">";
+						productList += "안심결제";
+						productList += "</li>";
+					}
+
+					productList += "            </ul>";
+					productList += "            <div class=\"seller-info\">";
+					productList += "                <div class=\"info-label\">판매자</div>";
+
+					productList += "                <div class=\"seller-rank ";
+					if (item.user_rank === "member") {
+						productList +=	"fa fa-star-o\"></div>";
+					}
+					else if (item.user_rank === "silver") {
+						productList +=	"fa fa-star\"></div>";
+					}
+					else if (item.user_rank === "gold") {
+						productList +=	"fa fa-diamond\"></div>";
+					}
+					else if (item.user_rank === "admin") {
+						productList +=	"fa fa-user-circle-o\"></div>";
+					}
+					else if (item.user_rank === "blackList") {
+						productList +=	"fa fa-frown-o\"></div>";
+					}
+
+					productList += "                <div class=\"seller-name\">";
+					productList += item.user_name;
+					productList += "                </div>";
+					productList += "            </div>";
+					productList += "        </div>";
+					productList += "    </div>";
+					productList += "    <div class=\"board-info\">";
+					productList += "        <div class=\"like-count\">";
+					productList += "        </div>";
+					productList += "        <div class=\"comment\">";
+					productList += "            <span class=\"fa fa-commenting-o\"></span>";
+					productList += "            10";
+					productList += "        </div>";
+					productList += "        <div class=\"complain\">";
+					productList += "            <span class=\"fa fa-thumbs-o-down\"></span>";
+					productList += "            10";
+					productList += "        </div>";
+					productList += "        <div class=\"hits\">";
+					productList += "            <span class=\"fa fa-eye\"></span>";
+					productList += "            1,0";
+					productList += "        </div>";
+					productList += "        <div class=\"reporting-date\">";
+					productList += "            <span class=\"fa fa-clock-o\"></span>";
+					productList += common.getFormatDate(item.update_date);
+					productList += "        </div>";
+					productList += "    </div>";
+
+					if(menuCategory === "processing" || menuCategory === "complain") {
+						productList += "<div class=\"list-selector\" check-box=\"\" name=\"chk\" get-checked=\"false\"></div>";
+					}
+
+					productList += "</li>";
+					initCountLike(item.product_id);
+					initHits(item.product_id);
+				}
+
+
+				$(".product-list").append(productList);
+
+				goMarketDetail();
+				/* 더보기 클릭시*/
+				$(".more-list").off();
+				$(".more-list").on("click", function () {
+					var brandId = $(".menu-category li.active").attr("brand-id");
+					++page;
+					if (page<=lastPage) {
+						productListAjax(brandId);
+					}
+					else {
+						alert("마지막 물품입니다.");
+					}
+				});
+			},
+			error: function (jqXHR) {
+				alert(jqXHR.responseJSON.message + "메뉴: " + menuCategory );
+			},
+		});
+	}
+
+
 
 	/*function settingURL() {
 		var tabMenuId = $(".menu-category>ul .active").attr("menu-category-detail");
@@ -11,28 +214,41 @@ require([
 	}*/
 
 	var naviHandler = function (jqElement) {
-		if ($(jqElement).attr("menu-category-detail") === "complete") {
+		var menuCategory = $(jqElement).attr("menu-category-detail");
+
+		/*
+		 디폴트 : selling (거래 가능한 상태)
+		 거래중 : processing (거래 진행 중인 상태)
+		 판매완료 : complete (판매 완료)
+		 블라인드 : blind
+		 */
+		if (menuCategory === "complete") {
 			$(".admin-product .all-selected").css("display", "none");
 			$(".admin-product>section.processing").css("display", "none");
 			$(".admin-product>section.complain").css("display", "none");
 			$(".admin-product>section.complete").css("display", "block");
 		}
-		else if ($(jqElement).attr("menu-category-detail") === "processing") {
+		else if (menuCategory === "processing") {
 			$(".admin-product .all-selected").css("display", "block");
 			$(".admin-product>section.complete").css("display", "none");
 			$(".admin-product>section.complain").css("display", "none");
 			$(".admin-product>section.processing").css("display", "block");
 			common.listSelector();
 		}
-		else if ($(jqElement).attr("menu-category-detail") === "complain") {
+		else if (menuCategory === "complain") {
 			$(".admin-product .all-selected").css("display", "block");
 			$(".admin-product>section.complete").css("display", "none");
 			$(".admin-product>section.processing").css("display", "none");
 			$(".admin-product>section.complain").css("display", "block");
 			common.listSelector();
 		}
+
+		/* 리스트 초기화*/
+		$(".product-list").html("");
+		showList(menuCategory);
 	};
 
+	showList("complete");
 	$(".menu-category>ul>li").on("click", function() {
 		common.navigate(this, naviHandler);
 	});
--- src/js/admin-update-member.js
+++ src/js/admin-update-member.js
@@ -3,20 +3,7 @@ require([
 ], function() {
 	var common = require("common");
 
-	//수정 버튼 클릭 시 확인 팝업 창
-	$(".btn-update").on("click", function() {
-		common.popUp("pop-up-series", "left");
-		$(".pop-up-series").css("position", "fixed");
-		$(".pop-up-series>.text1").text("회원 정보를 수정하시겠습니까?");
-	});
 
-	//취소 버튼 클릭 시 취소확인 팝업 창
-	$(".btn-cancel").on("click", function() {
-		common.popUp("pop-up-series", "left");
-
-		$(".pop-up-series").css("position", "fixed");
-		$(".pop-up-series>.text1").text("수정을 취소하시겠습니까?");
-	});
 
 	// 드롭다운 메뉴들
 	function dropDown() {
@@ -59,35 +46,93 @@ require([
 		});
 	}
 
-	/*-----회원 정보 수정-----*/
-	$(".pop-up-series .btn.popup-ok").on("click", function() {
-		var userUid = common.getQuerystring("uid");
-		var userName = $("input#userName[value]").val().trim();
-		var userRank = $("#userRank .dropdown-selected").attr("s-value").trim();
-		var userPhoneNum = $("input#userPhoneNum[value]").val().trim();
-		var userAddr = $("input#userAddr[value]").val().trim();
-		var userAddrDetail = $("input#userAddrDetail[value]").val().trim();
-		var userZipCode = $("input#userZipCode[value]").val().trim();
 
-		$.ajax({
-			url: window._ctx.root + "/api/admin/member/" + userUid,
-			method: "PUT",
-			data: {
-				userName: userName,
-				userRank: userRank,
-				userPhoneNum: userPhoneNum,
-				userAddr: userAddr,
-				userAddrDetail: userAddrDetail,
-				userZipCode: userZipCode,
-			},
-			success: function() {
-				location.href = window._ctx.root + "/admin/admin-member.html";
-			},
-			error: function() {
-				alert("수정에 실패했습니다.");
-			},
+
+	loadUserData();
+
+	//수정 버튼 클릭 시 확인 팝업 창
+	$(".btn-update").on("click", function() {
+		common.popUp("pop-up-series", "left");
+		$(".pop-up-series").css("position", "fixed");
+		$(".pop-up-series>.text1").text("회원 정보를 수정하시겠습니까?");
+
+
+		/*-----회원 정보 수정-----*/
+		$(".pop-up-series .btn.popup-ok").on("click", function() {
+			var userUid = common.getQuerystring("uid");
+			var userName = $("input#userName[value]").val().trim();
+			var userRank = $("#userRank .dropdown-selected").attr("s-value").trim();
+			var userPhoneNum = $("input#userPhoneNum[value]").val().trim();
+			var userAddr = $("input#userAddr[value]").val().trim();
+			var userAddrDetail = $("input#userAddrDetail[value]").val().trim();
+			var userZipCode = $("input#userZipCode[value]").val().trim();
+
+			$.ajax({
+				url: window._ctx.root + "/api/admin/member/" + userUid,
+				method: "PUT",
+				data: {
+					userName: userName,
+					userRank: userRank,
+					userPhoneNum: userPhoneNum,
+					userAddr: userAddr,
+					userAddrDetail: userAddrDetail,
+					userZipCode: userZipCode,
+				},
+				success: function() {
+					location.href = window._ctx.root + "/admin/admin-member.html";
+				},
+				error: function() {
+					alert("수정에 실패했습니다.");
+				},
+			});
 		});
 	});
 
-	loadUserData();
+
+
+	//삭제 버튼 클릭 시 확인 팝업 창
+	$(".btn-delete").on("click", function() {
+		common.popUp("pop-up-series", "left");
+		$(".pop-up-series").css("position", "fixed");
+		$(".pop-up-series>.text1").css("width", "55%");
+		$(".pop-up-series>.text1").css("top", "20%");
+		$(".pop-up-series>.text1").css("left", "25%");
+		$(".pop-up-series>.text1").css("color", "#cc0033");
+		$(".pop-up-series>.text1").text("해당 회원을 \n강제로 탈퇴 시키겠습니까?");
+
+
+		//팝업창 확인 버튼 클릭 시
+		/*-----회원 강제 탈퇴-----*/
+		$(".pop-up-series .btn.popup-ok").on("click", function() {
+			var userUid = common.getQuerystring("uid");
+
+			$.ajax({
+				url: window._ctx.root + "/api/admin/member/" + userUid,
+				method: "DELETE",
+				success: function() {
+					location.href = window._ctx.root + "/admin/admin-member.html";
+				},
+				error: function() {
+					alert("삭제에 실패했습니다.");
+				},
+			});
+		});
+
+		//팝업창 취소 버튼 클릭 시
+		$(".pop-up-series .popup-close").on("click", function() {
+			$(".pop-up-series>.text1").css("width", "100%");
+			$(".pop-up-series>.text1").css("top", "30%");
+			$(".pop-up-series>.text1").css("left", "0");
+			$(".pop-up-series>.text1").css("color", "#50546d");
+		});
+	});
+
+
+	//취소 버튼 클릭 시 취소확인 팝업 창
+	$(".btn-cancel").on("click", function() {
+		common.popUp("pop-up-series", "left");
+
+		$(".pop-up-series").css("position", "fixed");
+		$(".pop-up-series>.text1").text("수정을 취소하시겠습니까?");
+	});
 });
